0946d2655e1ddba9f21f8bfd5b5637ea04a23606
Abhishekgowda KM <34267@ITCINFOTECH.COM>
2020-06-16 15:19:57 +0530

    Updates

---
+++ precache-manifest.ddf5878bc6e38d19ef125091b03ced50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2d0601c816a203a341ca3048bf3417",
+    "url": "/React-Redux-Todo-list/index.html"
+  },
+  {
+    "revision": "1f7e6490b5859a98d350",
+    "url": "/React-Redux-Todo-list/static/css/main.821bd216.chunk.css"
+  },
+  {
+    "revision": "473f2bd8bf9259443ebd",
+    "url": "/React-Redux-Todo-list/static/js/2.a641a9bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-Todo-list/static/js/2.a641a9bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f7e6490b5859a98d350",
+    "url": "/React-Redux-Todo-list/static/js/main.01c47211.chunk.js"
+  },
+  {
+    "revision": "fdca2772534da0c6106c",
+    "url": "/React-Redux-Todo-list/static/js/runtime-main.1626c9d4.js"
+  }
+]);
